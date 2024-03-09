fcc06dd1abfdf305d260aad04a3c997a8d432a1e
Umesh Chandra Vankadhara <umeshch.vankadhara@inviz.ai>
2024-03-09 12:13:12 +0530

    updates

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: window.location.origin,
+        // target: "http://localhost:5000",
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
