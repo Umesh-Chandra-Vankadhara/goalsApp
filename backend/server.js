01a10617631a80a27672678f0e55061219324d76
Umesh Chandra Vankadhara <umeshch.vankadhara@inviz.ai>
2024-03-09 12:08:31 +0530

    updates

--- backend/server.js
+++ backend/server.js
@@ -17,13 +17,17 @@ app.use(errorHandler);
 
 // serve frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static.apply(path.join(__dirname, "../frontend/dist")));
+  // app.use(express.static.apply(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "dist", "index.html")
-    )
-  );
+  // app.get("*", (req, res) =>
+  //   res.sendFile(
+  //     path.resolve(__dirname, "../", "frontend", "dist", "index.html")
+  //   )
+  // );
+  app.get("/", (req, res) => {
+    app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
 }
