09fd6dd0690de50428b187fe056239783a4b5da3
Umesh Chandra Vankadhara <umeshch.vankadhara@inviz.ai>
2024-03-09 12:33:13 +0530

    updates

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const target = "http://localhost:5000";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: window.location.origin,
-        // target: "http://localhost:5000",
+        target,
         changeOrigin: true,
         // rewrite: (path) => path.replace(/^\/api/, ""),
       },
